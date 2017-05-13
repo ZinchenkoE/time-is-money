4549325b3d3e08d0202c940711ec5fd9692088ea
EZI <380506487966@yandex.ua>
2017-05-13 11:39:34 +0300

    code review

--- server/config/index.js
+++ server/config/index.js
@@ -2,6 +2,6 @@ var nconf = require('nconf');
 var path = require('path');
 nconf.argv()
     .env()
-    .file({ file: path.join(__dirname, 'config.json' )});
+    .file({file: path.join(__dirname, 'config.json')});
 
 module.exports = nconf;
--- server/main.js
+++ server/main.js
@@ -5,32 +5,34 @@ var express = require('express');
 var app = express();
 var connection;
 
-function initConnection () {
+function initConnection() {
     console.log('_____init db connection____');
     connection = mysql.createConnection({
-        host     : config.get('dbHost'),
-        user     : config.get('dbUser'),
-        password : config.get('dbPassword'),
-        database : config.get('dbName')
+        host: config.get('dbHost'),
+        user: config.get('dbUser'),
+        password: config.get('dbPassword'),
+        database: config.get('dbName')
     });
-    connection.connect(function() {
+    connection.connect(function () {
         connection.query(
             'SET NAMES utf8 COLLATE utf8_general_ci',
-            function (err) { if (err) throw err; }
+            function (err) {
+                if (err) throw err;
+            }
         );
     });
-    connection.on('error', function(err) {
+    connection.on('error', function (err) {
         console.log('db error: ' + err.code, err);
-        if(err.code === 'PROTOCOL_CONNECTION_LOST') initConnection();
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') initConnection();
         else throw err;
     });
 }
-try{
+try {
     initConnection();
 
     app.use(bodyParser.json());
     app.use(express.static('dist'));
-    app.use(function(req, res, next) {
+    app.use(function (req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         res.header("Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS");
@@ -41,13 +43,13 @@ try{
         res.send('Hello!!!');
     });
     app.get('/comments', function (req, res) {
-        var page   = req.query.page   || 1;
+        var page = req.query.page || 1;
         var sortBy = req.query.sortBy || 'comment_id';
-        var order  = req.query.order ===  'ASC' ? 'ASC' : 'DESC';
+        var order = req.query.order === 'ASC' ? 'ASC' : 'DESC';
         var offset = (page - 1) * 25;
 
         var query = 'SELECT `comment_id`, `parent_id`, `username`, `email`, `text`, `create_time`, `homepage` ' +
-                    'FROM `tim_comments` ORDER BY `' + sortBy + '` ' + order + ' LIMIT 25 OFFSET ' + offset;
+            'FROM `tim_comments` ORDER BY `' + sortBy + '` ' + order + ' LIMIT 25 OFFSET ' + offset;
         connection.query(query, function (err, comments) {
             if (err) throw err;
             connection.query('SELECT COUNT(*) AS comments_count FROM `tim_comments`', function (err, rows) {
@@ -64,16 +66,19 @@ try{
     app.post('/comments', function (req, res) {
         console.log('Пришел post', req.body);
         var data = req.body;
-        data.parent_id   = data.parent_id    || 0;
-        data.username    = data.username     || '';
-        data.email       = data.email        || '';
-        data.text        = data.text         || '';
-        data.create_time = data.create_time  || +new Date();
-        data.browser     = data.browser      || '';
-        data.ip          = req.ip            || '';
+        data.parent_id = data.parent_id || 0;
+        data.username = data.username || '';
+        data.email = data.email || '';
+        data.text = data.text || '';
+        data.create_time = data.create_time || +new Date();
+        data.browser = data.browser || '';
+        data.ip = req.ip || '';
 
         connection.query('INSERT INTO tim_comments SET ?', data, function (err) {
-                if (err) { res.end(); throw err; }
+                if (err) {
+                    res.end();
+                    throw err;
+                }
                 res.end('success');
             }
         );
@@ -83,24 +88,6 @@ try{
     app.listen(config.get('port'), function () {
         console.log('App listening on port ' + config.get('port'))
     });
-}catch (e){
+} catch (e) {
     console.log('ERROR', e);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
